94aafd8e3ebd9ec4a827a5edad82cabb8d1eaf08
HengHeng <103663464+FOX1224@users.noreply.github.com>
2024-05-10 00:57:09 +0800

    Update Children.java

--- src/Children.java
+++ src/Children.java
@@ -1,53 +1,60 @@
 import java.util.Scanner;
 
 // it is a sub class that the parents class is Customer
-public class Children extends Customer {
-    private double childPrice;
-    private final double DISCOUNT = 0.5;
+public class Children extends Customer
+{
+  private double childPrice;
+  private final double DISCOUNT = 0.5;
 
-    // no args constructor
-    public Children() {
+  // no args constructor
+  public Children (){}
+
+  // Constructor with parameters
+    public Children(int adultQuantity, int childQuantity, int studentQuantity) {
+        super(adultQuantity, childQuantity, studentQuantity);
     }
 
-    // constructor with three parameters
-    public Children(int adultQauntity, int childQuantity, int studentQuantity) {
-        super(adultQauntity, childQuantity, studentQuantity);
-    }
-
-    // validation method that use to validate input by user
-    public int input(int totalQty) {
+  // validation method that use to validate input by user
+  @Override
+    public int validateInput(int totalQty, boolean isTotal) {
+        Scanner sc = new Scanner(System.in);
         int qty;
+        String message = "quantity of children tickets needed";
 
         do {
-            Scanner sc = new Scanner(System.in);
-            System.out.print("\nEnter quantity of children tickets needed: ");
+            System.out.print("\nEnter " + message + ": ");
             while (!sc.hasNextInt()) {
-                System.out.println("Invalid input! Please enter NUMBERS ONLY!");
-                System.out.print("\nEnter quantity of children tickets needed: ");
+                System.out.println("Invalid input! Please enter numbers only!");
+                System.out.print("\nEnter " + message + ": ");
                 sc.next();
             }
             qty = sc.nextInt();
-            if (super.inputValidation(qty, totalQty)) {
+
+            if (!isTotal && (qty < 0 || qty > totalQty)) {
+                System.out.println("Ticket count cannot be more than total quantity of tickets needed!");
+            } else {
                 break;
             }
+        } while (true);
 
-        } while (qty < 0 || qty > totalQty);
         return qty;
     }
 
-    // override the method that found in parent class to calculate child price
+
+  // override the method that found in parent class to calculate child price
+  @Override
+  public double calPrice() {
+       this.childPrice = super.calPrice() * DISCOUNT;
+       return childPrice;
+    }
+
+    // Method to calculate total price of child tickets
     @Override
-    public double calPrice() {
-        this.childPrice = super.calPrice() * DISCOUNT;
-        return childPrice;
-    }
-
-    // calculate total child price
     public double calTotalPrice() {
-        return calPrice() * (double) super.getChildQuantity();
+        return calPrice() * super.getChildQuantity();
     }
 
-    // display the unit price as well as total price in each category by using toString method
+    // Method to display the unit price and total price of child tickets
     @Override
     public String toString() {
         return String.format("%.2f", calPrice()) + "\t\t\t|" + String.format("%.2f", calTotalPrice());
